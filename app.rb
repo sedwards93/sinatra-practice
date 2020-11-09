de29f455eb95005ebcf237c4a67c15385924c3fe
Symion Edwards <symion.edwards@gmail.com>
2020-11-09 16:39:35 +0000

    Added an instance variable to the route

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ get '/sym' do
 end
 
 get '/cat' do
-  "<div>
-   <img src ='http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png' style= 'border: 10px dashed red'>
-   </div>"
+@names = ["Amigo", "Oscar", "Viking"].sample
+ erb(:index)
+
 end
