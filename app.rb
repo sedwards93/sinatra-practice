bfe0c82542a33a160dc1713dd4f01a2414a427e6
Symion Edwards <symion.edwards@gmail.com>
2020-11-09 16:08:58 +0000

    Added a cat route with an image of a cat, using HTML and CSS

--- app.rb
+++ app.rb
@@ -13,3 +13,9 @@ end
 get '/sym' do
   'iodfgdfgdfgn'
 end
+
+get '/cat' do
+  "<div>
+   <img src ='http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png' style= 'border: 10px dashed red'>
+   </div>"
+end
